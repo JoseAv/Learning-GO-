818f0e0b03aa6fcd18a1d21d1473a130cb90a524
JoseAv <jm.aranav@gmail.com>
2026-01-07 23:06:23 -0600

    Terminado fucniones

--- theory/1.introduccion/main.go
+++ theory/1.introduccion/main.go
@@ -1,11 +1,14 @@
 package main
 
-import (
-	my "basic-golan/mymath"
-	"fmt"
-)
+import "fmt"
 
-func main() {
-	fmt.Println("Hellow world")
-	fmt.Println(my.Plus(2,3))
+// func main() {
+// 	fmt.Println("Hellow world")
+// 	fmt.Println(my.Plus(2,3))
+// 	fmt.Println(math.Pow(2,3))
+// }
+
+func main(){
+	var x int = 5
+	fmt.Println(x)
 }
